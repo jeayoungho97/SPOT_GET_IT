c6175a8027420f8148a17bac565941be95fe7acc
구진영 <koo001602@naver.com>
2026-04-27 16:25:43 +0900

    feat(rl): 서보모터 응답 지연 반영 코드 추가

--- ai_training/rl/legged_gym/legged_gym/envs/spotmicro_test/spotmicro_test_config.py
+++ ai_training/rl/legged_gym/legged_gym/envs/spotmicro_test/spotmicro_test_config.py
@@ -111,6 +111,8 @@ class SpotmicroTestCfg(LeggedRobotCfg):
         push_robots = True
         push_interval_s = 15
         max_push_vel_xy = 0.2
+        action_delay = True
+        action_delay_range = [0,1]
 
 
 class SpotmicroTestCfgPPO(LeggedRobotCfgPPO):
